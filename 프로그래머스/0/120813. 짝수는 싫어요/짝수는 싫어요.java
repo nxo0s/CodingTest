e249c3ee3cd647e18e93ac4c76acbd61254031d7
서정 <s2626@e-mirim.hs.kr>
2026-07-29 15:36:25 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n) {
+        int count = (n + 1) / 2;
+        int[] answer = new int[count];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = i * 2 + 1;
+        }
+
+        return answer;
+    }
+}
